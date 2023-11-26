673e3a8325f8a133732d73f6a080e526cf176008
Confaa <juanconfaa@gmail.com>
2023-11-26 17:55:10 -0300

    Format Code

--- src/routes/permisions.router.js
+++ src/routes/permisions.router.js
@@ -4,13 +4,12 @@ import { userService } from "../dao/repositories/index.js";
 const router = Router();
 
 router.get("/premium/:uid", async (req, res) => {
-    let uid = req.params.uid;
-    let user= await userService.getUserById(uid);
+  let uid = req.params.uid;
+  let user = await userService.getUserById(uid);
   if (req.user.role === "premium") {
-      await userService.updateUser(user.email, {role:"user"});
+    await userService.updateUser(user.email, { role: "user" });
   } else {
-      await userService.updateUser(user.email, {role:"premium"});
-
+    await userService.updateUser(user.email, { role: "premium" });
   }
 });
 export default router;
